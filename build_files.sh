4a04d07dd51c09a9ee329056931e4267ca3f9ae2
ry44an <ryanzander99@gmail.com>
2026-02-02 20:13:23 -0500

    fix site domain

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pip install -r webapp/requirements.txt
-cd webapp && python manage.py migrate --noinput && python manage.py collectstatic --noinput
+cd webapp && python manage.py migrate --noinput && python manage.py shell -c "from django.contrib.sites.models import Site; Site.objects.update_or_create(id=1, defaults={'domain': 'clear25.xyz', 'name': 'C.L.E.A.R.'})" && python manage.py collectstatic --noinput
